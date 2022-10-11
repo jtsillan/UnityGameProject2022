f3937a9ef0f1217fa5129a93b881b9765976738a
MiskaVirtanen <miska.virtanen@edu.lapinamk.fi>
2022-10-11 19:51:23 +0300

    demon_aloitus_ja_importtauksia
    pelaajan ja kameran liike ja assettien importtauksia.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+
+    //eteisyys pelaajasta
+    Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.position;
+    }
+
+
+    //kameran seuranta ja keskittäminen
+    private void Update()
+    {
+
+        Vector3 targetPos = player.position + offset;
+        targetPos.x = 0;
+        transform.position = targetPos;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //hahmon nopeus
+    public float speed = 5;
+
+    //hahmon objekti
+    public Rigidbody rb;
+
+    //sivuttaisliike ja sen nopeutus
+    float horizontalInput;
+    public float horizontalMultiplier = 2;
+
+
+    private void FixedUpdate()
+    {
+        //liike eteenpäin
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        //liike sivuttain
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+        //hahmon objektin liikutus
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+
+    }
+
+
+
+    private void Update()
+    {
+        //sivuttais liikkeen kontrolli
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+}
