d692fc0faf716e7ca6e86be9860bd76c9f730f43
jtsillan <juha.sillanpaa@edu.lapinamk.fi>
2022-12-07 10:00:11 +0200

    Edit TestSceneTileFunctions
    - clean script from old comments

--- Assets/Scripts/TestSceneSripts/TestSceneTileFunctions.cs
+++ Assets/Scripts/TestSceneSripts/TestSceneTileFunctions.cs
@@ -28,50 +28,14 @@ public class TestSceneTileFunctions : MonoBehaviour
     }
 
 
-    /*
-    private void OnBecameInvisible()
-    {
-        DestroyImmediate(gameObject);
-        generatedTiles.MakeNewFlatLast();
-    }
-    */
-
-    /*
-       private void OnBecameVisible()
-       {
-          //generatedTiles.MakeNewUpHillLast();
-          //generatedTiles.MakeNewFlatLast();
-          //generatedTiles.MakeNewDownHillLast();
-          generatedTiles.MakeNewFlatLast();
-           Debug.Log("On Become Visible --> Make New Tiles");
-       }
-      */
-
-
     void OnCollisionEnter(Collision collision)
     {
         //Etsii tagill? gameobjectin
         if (collision.gameObject.tag == "TileDeleter")
         {
-            //hae kent�st� gameobjekti tagilla "Tile" ja "Obstacle"
-            // laske yhteen niiden pituudet
-            // jos pituudet < maksimim��r� tilej� kent�ss� suorita makenewflatlast()
-            // GameObject[] tiles = GameObject.FindWithTag("Tile");
-            // GameObject[] obstacles = GameObject.FindWithTag("Obstacle");
-            /* if ((tiles.Lenght + obstacles.Lenght) < 25)
-             * {
-             *       generatedTiles.MakeNewFlatLast();
-             * }
-             */
-             /*
-             UnityEditor.EditorApplication.delayCall += () =>
-             {
-             };
-             */
+            
             generatedTiles.MakeNewFlatLast();
             destroyMe = true;
-
         }
-    }  
-    
+    }      
 }
