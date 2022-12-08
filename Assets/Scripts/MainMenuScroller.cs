f7787aaecd9b2d2406bfadb6c06860d5dec37d54
Dhaakila <dhaakila@edu.lapinamk.fi>
2022-12-08 06:18:01 +0200

    FinalMenu
    +MainMenu fully works with customController.
    
    ===NEEDS TESTING===

--- Assets/Scripts/MainMenuScroller.cs
+++ Assets/Scripts/MainMenuScroller.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 using System.Threading;
+using UnityEngine.UI;
 public class MainMenuScroller : MonoBehaviour
 {
     public TMP_Text[] selectionLetters;
     public int currentLetter;
     private int currentLetterSelection = 1;
+    public Slider mSlider;
     public GameObject OptionsMenu;
     public GameObject TutorialMenu;
-    public GameObject TutorialBackButton;
+    public GameObject OptionsVolumeButton;
     public GameObject OptionsBackButton;
     public GameObject ArrowPlay;
     public GameObject ArrowOptions;
     public GameObject ArrowTutorial;
     public GameObject BackArrowTutorial;
     [SerializeField] CustomController customController;
-    private string[] letters = {"_","Play", "Options", "Tutorial","_","Back1"};
+    private string[] letters = {"_","Play", "Options", "Tutorial","_","Back1","Slider","SliderBack"};
     // Start is called before the first frame update
     private void Start()
     {
@@ -28,65 +30,88 @@ public class MainMenuScroller : MonoBehaviour
         GameObject input = GameObject.Find("ButtonManager");
         customController = input.GetComponent<CustomController>();
         ArrowPlay.SetActive(true);
+        mSlider.value = -15;
+        mSlider.maxValue = 0;
     }
 
     private void Update()
     {   
+            //JOS VALINTA MENEE TUTORIAALISTA ALAS, SE MENEE PLAY NAPPIIN
             if (currentLetter == 4)
             {
-
                 currentLetter -= 3;
             }
 
+            //JOS VALINTA MENEE PLAYSTA YLÖS, SE MENEE TUTORIAL NAPPIIN
             if (currentLetter == 0)
             {
-
                 currentLetter += 3;
             }
             
+            //JOS ARROW OSOITTAA PLAY NAPPIA -->
             if (currentLetter == 1)
             {
                 ArrowPlay.SetActive(true);
                 ArrowOptions.SetActive(false);
                 ArrowTutorial.SetActive(false);
-                if (Input.GetKeyDown(KeyCode.Space))
+                if (Input.GetKeyDown(KeyCode.Space) || customController.buttonOnePressed == true && customController.buttonTwoPressed == true)
                 {
                     ArrowPlay.SetActive(false);
                     SceneManager.LoadScene("StartScene");
                 }
             }
 
+            if (currentLetter == 7)
+            {
+                if (Input.GetKeyDown(KeyCode.Space) || customController.buttonOnePressed == true && customController.buttonTwoPressed == true)
+                {
+                    OptionsVolumeButton.SetActive(true);
+                    OptionsBackButton.SetActive(false);
+                    OptionsMenu.SetActive(false);
+                    currentLetter -= 6;
+                }
+                
+            }
+
+            //LIIKKUVUUS OPTIONS MENUSSA -->
             if (GameObject.Find("Options") == true)
             {
-                if (Input.GetKeyDown(KeyCode.UpArrow))
+                if (Input.GetKeyDown(KeyCode.UpArrow) || customController.buttonOnePressed == true)
                 {
+                    OptionsVolumeButton.SetActive(true);
+                    OptionsBackButton.SetActive(false);
+                    Debug.Log(currentLetter);
+                }
 
-                    TutorialMenu.SetActive(false);
-                    ArrowPlay.SetActive(false);
-                    ArrowTutorial.SetActive(false);
-                    ArrowOptions.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.DownArrow) || customController.buttonTwoPressed == true)
+                {
+                    OptionsVolumeButton.SetActive(false);
+                    OptionsBackButton.SetActive(true);
                     Debug.Log(currentLetter);
                 }
             }
+
+            //JOS ARROW OSOITTAA OPTIONS NAPPIA -->
             if (currentLetter == 2)
             {
             ArrowPlay.SetActive(false);
             ArrowOptions.SetActive(true);
             ArrowTutorial.SetActive(false);
-                if (Input.GetKeyDown(KeyCode.Space))
+                if (Input.GetKeyDown(KeyCode.Space) || customController.buttonOnePressed == true && customController.buttonTwoPressed == true)
                 {
                     OptionsMenu.SetActive(true);
                     if (GameObject.Find("Options") == true)
                     {
                         currentLetter += 4;
-                        Debug.Log(currentLetter);
+                        Thread.Sleep(100);
                     }
                 }
             }
 
+            //PIILOTTAA TUTORIAL MENUN, JOS SEN SISÄLLÄ TEHDÄÄN VALINTA
             if (GameObject.Find("Tutorial") == true)
             {
-                if (Input.GetKeyDown(KeyCode.Space))
+                if (Input.GetKeyDown(KeyCode.Space) || customController.buttonOnePressed == true && customController.buttonTwoPressed == true)
                 {
                     currentLetter -= 4;
                     TutorialMenu.SetActive(false);
@@ -96,13 +121,13 @@ public class MainMenuScroller : MonoBehaviour
                 }
             }
 
-
+            //KUN ARROW OSOITTAA TUTORIAL NAPPIA -->
             if (currentLetter == 3)
             {
             ArrowPlay.SetActive(false);
             ArrowOptions.SetActive(false);
             ArrowTutorial.SetActive(true);
-                if (Input.GetKeyDown(KeyCode.Space))
+                if (Input.GetKeyDown(KeyCode.Space) || customController.buttonOnePressed == true && customController.buttonTwoPressed == true)
                 {
                     TutorialMenu.SetActive(true);
                     if (GameObject.Find("Tutorial") == true)
@@ -113,26 +138,38 @@ public class MainMenuScroller : MonoBehaviour
                 }
             }
 
+            // KUN ARROW OSOITTAA VOLUMEA -->
+            if (currentLetter == 6)
+            {
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    mSlider.value -= 5;
+                    Thread.Sleep(100);
+                }
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    mSlider.value += 5;
+                    Thread.Sleep(100);
+                }
+            }
 
 
-        
-        if (Input.GetKeyDown(KeyCode.DownArrow) && currentLetter != 5)
+
+        //ALASPÄIN LIIKKUMINEN VALIKOSSA
+        if (Input.GetKeyDown(KeyCode.DownArrow) && currentLetter != 7 && currentLetter != 5)
         {
             currentLetterSelection++;
             currentLetter++;
             Thread.Sleep(100);
-            selectionLetters[currentLetter-1].color = Color.black;
-            selectionLetters[currentLetter].color = Color.red;
         }
-        //ALKAA TOINEN OSA
-        if (Input.GetKeyDown(KeyCode.UpArrow) && currentLetter != 5)
+
+        //YLÖSPÄIN LIIKKUMINEN VALIKOSSA
+        if (Input.GetKeyDown(KeyCode.UpArrow) && currentLetter != 6 && currentLetter != 5)
         {
             currentLetterSelection--;
             currentLetter--;
             Thread.Sleep(100);
-            selectionLetters[currentLetter+1].color = Color.black;
-            selectionLetters[currentLetter].color = Color.red;
         }
-        //LOPPUU TOINEN OSA
+
     }
 }
