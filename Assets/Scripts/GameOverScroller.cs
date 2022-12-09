f966c7520441f5be90363624913349a6312a6b28
Dhaakila <dhaakila@edu.lapinamk.fi>
2022-12-09 02:13:36 +0200

    GameoverTimer
    +Added timer to gameover screen to prevent players from accidentally restarting the game after scene switch

--- Assets/Scripts/GameOverScroller.cs
+++ Assets/Scripts/GameOverScroller.cs
@@ -8,11 +8,13 @@ using UnityEngine.UI;
 public class GameOverScroller : MonoBehaviour
 {
     public TMP_Text[] selectionLetters;
+    public TMP_Text TimerTxt;
     public int currentLetter;
     private int currentLetterSelection = 0;
     public GameObject RestartArrow;
     public GameObject MainMenuArrow;
-
+    public bool TimerOn = false;
+    public float targetTime = 2.0f;
     [SerializeField] CustomController customController;
     private string[] letters = {"Restart", "MainMenu"};
     // Start is called before the first frame update
@@ -29,16 +31,23 @@ public class GameOverScroller : MonoBehaviour
 
     private void Update()
     {   
-            //JOS ARROW OSOITTAA RESTART NAPPIA -->
+
+        targetTime -= Time.deltaTime;
+        if (targetTime <= 0.0f)
+        {
+        timerEnded();
+        }
+
+        void timerEnded()
+        {
+                //JOS ARROW OSOITTAA RESTART NAPPIA -->
             if (currentLetter == 0)
             {
                 if (Input.GetKeyDown(KeyCode.Space) || customController.hallValue == true)
                 {
                     SceneManager.LoadScene("StartScene");
                 }
-            }
-
-
+            }         
             //JOS ARROW OSOITTAA MAINMENU NAPPIA -->
             if (currentLetter == 1)
             {
@@ -46,28 +55,28 @@ public class GameOverScroller : MonoBehaviour
                 {
                     SceneManager.LoadScene("Menu");
                 }
+            }   
+            //ALASPÄIN LIIKKUMINEN VALIKOSSA
+            if (Input.GetKeyDown(KeyCode.DownArrow) && currentLetter != 1 || customController.buttonTwoPressed == true && currentLetter != 1)
+            {
+                currentLetterSelection++;
+                currentLetter++;
+                RestartArrow.SetActive(false);
+                MainMenuArrow.SetActive(true);
+                Debug.Log(currentLetter);
+                Thread.Sleep(150);
             }
 
-
-        //ALASPÄIN LIIKKUMINEN VALIKOSSA
-        if (Input.GetKeyDown(KeyCode.DownArrow) || customController.buttonTwoPressed == true && currentLetter != 1)
-        {
-            currentLetterSelection++;
-            currentLetter++;
-            RestartArrow.SetActive(false);
-            MainMenuArrow.SetActive(true);
-            Thread.Sleep(150);
+            //YLÖSPÄIN LIIKKUMINEN VALIKOSSA
+            if (Input.GetKeyDown(KeyCode.UpArrow) && currentLetter != 0 || customController.buttonOnePressed == true && currentLetter != 0)
+            {
+                currentLetterSelection--;
+                currentLetter--;
+                RestartArrow.SetActive(true);
+                MainMenuArrow.SetActive(false);
+                Debug.Log(currentLetter);
+                Thread.Sleep(150);
+            }
         }
-
-        //YLÖSPÄIN LIIKKUMINEN VALIKOSSA
-        if (Input.GetKeyDown(KeyCode.UpArrow) || customController.buttonOnePressed == true && currentLetter != 0)
-        {
-            currentLetterSelection--;
-            currentLetter--;
-            RestartArrow.SetActive(true);
-            MainMenuArrow.SetActive(false);
-            Thread.Sleep(150);
-        }
-
     }
 }
